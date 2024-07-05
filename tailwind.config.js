5b31c014fc4229b083938a2e7fc07ddc77671a8d
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-07-05 13:51:07 +0200

    create html + tailwindcss form

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["templates/*.html"],
+  theme: {
+    colors:{
+      'limonkowy': '#a3e635'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
